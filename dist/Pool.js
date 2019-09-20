d15b84490dc14836b0307238ebbc40d78196d0dc
Janiel Oliveira <janielbelmont@gmail.com>
2019-09-20 12:09:56 -0300

    fix: include callback error on connection

--- dist/Pool.js
+++ dist/Pool.js
@@ -1,11 +1,10 @@
-
-const genericPool = require('generic-pool');
-const DbDriver = require('./sybase');
+const genericPool = require("generic-pool");
+const DbDriver = require("./sybase");
 
 /**
  * 数据库连接，线程池
- * 
- * @param {any} opts 
+ *
+ * @param {any} opts
  *      {
  *        *host,
  *        *port,
@@ -19,68 +18,80 @@ const DbDriver = require('./sybase');
  *       }
  */
 class DBPool {
-  constructor(opts) {
-    if (!opts.host || !opts.port) {
-      throw new Error('Sybase DB params lacked!')
-    }
-    this.name = opts.name || opts.host
-
-    const _create = () => {
-      return new Promise((resolve, reject) => {
-        var client = new DbDriver(
-          opts.host,
-          opts.port,
-          opts.dbname,
-          opts.username,
-          opts.password,
-          true,
-          opts.jarPath);
-        client.connect((err) => {
-          if (err) return reject(err)
-          return resolve(client);
-        });
-      })
-    }
-
-    const _destroy = (client) => {
-      return new Promise((resolve) => {
-        if (client.isConnected()) {
-          client.disconnect();
-          resolve();
+    constructor(opts) {
+        if (
+            !opts.host ||
+            !opts.port ||
+            !opts.dbname ||
+            !opts.username ||
+            !opts.password
+        ) {
+            throw new Error("Sybase DB params lacked!");
         }
-      })
+        this.name = opts.name || opts.host;
+        this.client = null;
+
+        const _create = () => {
+            return new Promise((resolve, reject) => {
+                this.client = new DbDriver(
+                    opts.host,
+                    opts.port,
+                    opts.dbname,
+                    opts.username,
+                    opts.password,
+                    true,
+                    opts.jarPath
+                );
+                this.client.connect((err) => {
+                    if (err) return reject(err);
+                    return resolve(this.client);
+                });
+            });
+        };
+
+        const _destroy = (client) => {
+            return new Promise((resolve) => {
+                if (client.isConnected()) {
+                    client.disconnect();
+                    resolve();
+                }
+            });
+        };
+
+        this.pool = genericPool.createPool(
+            {
+                create: _create,
+                destroy: _destroy
+            },
+            {
+                max: opts.max || 10,
+                min: opts.min || 2
+            }
+        );
     }
 
-    this.pool = genericPool.createPool({
-      create: _create,
-      destroy: _destroy
-    }, {
-        max: opts.max || 10,
-        min: opts.min || 2
-      });
-  }
+    _exe(connect, sql) {
+        return new Promise((resolve, reject) => {
+            connect.then((client) => {
+                client.query(sql, (err, data) => {
+                    if (err) {
+                        console.error("Errro on _exec: ", err);
+                        reject(err);
+                    } else {
+                        this.pool.release(client);
+                        resolve(data);
+                    }
+                });
+            });
+        });
+    }
 
-  _exe (connect, sql, callback) {
-    connect.then((client) => {
-      client.query(sql, (err, data) => {
-        this.pool.release(client);
-        callback(err, data)
-      })
-    })
-  }
+    async execute(sql) {
+        const connInfo = await this.pool.acquire();
+        return this._exe(connInfo, sql);
+    }
+}
 
-  execute (sql, callback) {
-    const connInfo = this.pool.acquire();
-    return new Promise((resolve, reject) => {
-      this._exe(connInfo, sql, (err, data) => {
-        callback && callback(err, data)
-        if (err) return reject(err);
-        return resolve(data);
-      })
-    })
-  }
-};
-
-module.exports = DBPool
-module.exports.DBPool = DBPool
-module.exports.default = DBPool
+module.exports = DBPool;
+module.exports.DBPool = DBPool;
+module.exports.default = DBPool;
--- exemple/app.js
+++ exemple/app.js
@@ -2,46 +2,56 @@ const SyBase = require("./db");
 SyBase.use(__dirname + "/models");
 
 const CRUD = () => {
-    SyBase.models.WXUser.by(SyBase.DBPools.main)
-        .create(
-            {
-                id: 12345678,
-                openid: "test Openid",
-                phone: "11",
-                headurl: "http://www.baidu.com/favicon",
-                cardno: "90000011",
-                wxname: 110
-            },
-            true
-        )
-        .then((res) => {
-            console.info("------- Created ", res);
-            return SyBase.models.WXUser.findByID(res.id);
-        })
-        .then((res) => {
-            console.info("------- Find BY ID : ", res);
-            return SyBase.models.WXUser.findOne({ id: res.id });
-        })
-        .then((res) => {
-            console.info("------- Find One : ", res);
-            return SyBase.models.WXUser.findAll({ id: res.id });
-        })
-        .then((res) => {
-            console.info("------- Find All : ", res);
-            return SyBase.models.WXUser.update(
-                { wxname: "updated name" },
-                { id: res[0].id }
-            );
-        })
-        .then((res) => {
-            console.info("------- Updated :", res);
-            return SyBase.models.WXUser.delete({ id: res.id });
-        })
-        .then(CRUD)
-        .catch((err) => {
-            console.error("********* Error :", err);
-            SyBase.models.WXUser.delete({ cardno: "90000011" });
-        });
+    SyBase.DBPools.main.client.connect((error) => {
+        if (error) {
+            console.error("error: ", error);
+        } else {
+            if (SyBase.DBPools.main.client.connected) {
+                SyBase.models.WXUser.by(SyBase.DBPools.main)
+                    .create(
+                        {
+                            id: 12345678,
+                            openid: "test Openid",
+                            phone: "11",
+                            headurl: "http://www.baidu.com/favicon",
+                            cardno: "90000011",
+                            wxname: 110
+                        },
+                        true
+                    )
+                    .then((res) => {
+                        console.info("------- Created ", res);
+                        return SyBase.models.WXUser.findByID(res.id);
+                    })
+                    .then((res) => {
+                        console.info("------- Find BY ID : ", res);
+                        return SyBase.models.WXUser.findOne({ id: res.id });
+                    })
+                    .then((res) => {
+                        console.info("------- Find One : ", res);
+                        return SyBase.models.WXUser.findAll({ id: res.id });
+                    })
+                    .then((res) => {
+                        console.info("------- Find All : ", res);
+                        return SyBase.models.WXUser.update(
+                            { wxname: "updated name" },
+                            { id: res[0].id }
+                        );
+                    })
+                    .then((res) => {
+                        console.info("------- Updated :", res);
+                        return SyBase.models.WXUser.delete({ id: res.id });
+                    })
+                    .then(CRUD)
+                    .catch((err) => {
+                        console.error("********* Error :", err);
+                        SyBase.models.WXUser.delete({ cardno: "90000011" });
+                    });
+            } else {
+                console.error("Sybase not connected!!!");
+            }
+        }
+    });
 };
 
 CRUD();
